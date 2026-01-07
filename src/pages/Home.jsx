8b294b585d5a485115ca03812036310f8e661adb
Sai Madhuri Bodapati <s.madhuri2k23@gmail.com>
2026-01-08 00:29:07 +0530

    added about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../sections/Hero.jsx'
+import About from '../sections/About.jsx'
 
 const Home = () => {
   return (
     <>
         <Hero />
+        <About />
     </>
   )
 }
---
+++ src/sections/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="h-screen bg-white rounded-tl-[60px] rounded-tr-[60px]">
+        <div className="about-text main-container py-4 lg:py-12 h-full flex justify-center items-center font-heading text-black text-2xl md:text-3xl xl:text-5xl leading-[1.25]">
+          My approach is driven by intentional design, scalable engineering, and
+          deep problem understanding. As a full-stack developer and an AI/ML
+          enthusiast, I build modern web experiences that balance performance,
+          usability, and long-term impact. I focus on creating work that looks
+          refined, functions seamlessly, and delivers real value through
+          thoughtful execution and attention to detail.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
